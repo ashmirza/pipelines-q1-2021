0f62f7ec629011c7df4a2db77d99ee36971ead7c
nitrosage <ashrafneo@gmail.com>
2021-01-21 02:39:24 +0000

    finished code

--- week3-2/spacex.py
+++ week3-2/spacex.py
@@ -1,5 +1,5 @@
 import requests
-
+import csv
 
 #This is where we will extract
 def get_launch_date(url):
@@ -10,11 +10,29 @@ def get_launch_date(url):
 
 #this is where we will transform
 def transform_data(launch_data):
-    
+    mission_names = []
+    missions_image = []
+    mission_id = []
+    # launch_success = []
+    # launch_failure = []
+    # payload_type = []
+
     for launch in launch_data:
-        print(launch["mission_name"])
+        mission_names.append(launch["mission_name"])
+        missions_image.append(launch["links"]["mission_patch"])
+        mission_id.append(launch["flight_number"])
+    #    launch_success.append(["launch_success"])
+    #    launch_failure.append(["launch_failure"])
+    #    payload.append(launch["payload"]["payload_type"])
+    #    print(launch["mission_name"])
+    with open('flightdata.csv', 'w', newline='') as f:
+        for a, b, c in zip(mission_id, mission_names, missions_image):
+            writer = csv.writer(f)
+            writer.writerow([a, b, c])
 
-
+    # print (len(mission_names))
+    # print (len(missions_image))
+    # print (len(mission_id))
 #This is where we will load
 
 
