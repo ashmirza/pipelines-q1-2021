73c6f752c37e147b50493eb69bea589f7b0db701
nitrosage <ashrafneo@gmail.com>
2021-01-21 01:56:01 +0000

    mission names working

--- week3-2/spacex.py
+++ week3-2/spacex.py
@@ -4,17 +4,21 @@ import requests
 #This is where we will extract
 def get_launch_date(url):
     launch_data = requests.get(url)
-    print(launch_data.content)
-
+    #print(launch_data.content)
+    return launch_data.json()
 
 
 #this is where we will transform
-
-
+def transform_data(launch_data):
+    
+    for launch in launch_data:
+        print(launch["mission_name"])
 
 
 #This is where we will load
 
 
 #This is where we run the code
-get_launch_date('https://api.spacexdata.com/v3/launches')
+result = get_launch_date('https://api.spacexdata.com/v3/launches')
+#print(len(result))
+transform_data(result)
