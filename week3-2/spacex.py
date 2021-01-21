8831e1900d09e92b7092f52689d73f4deb361d86
nitrosage <ashrafneo@gmail.com>
2021-01-21 01:27:47 +0000

    add spacex2

--- week3-2/spacex.py
+++ week3-2/spacex.py
@@ -1,4 +1,10 @@
+import requests
+
+
 #This is where we will extract
+def get_launch_date(url):
+    launch_data = requests.get(url)
+    print(launch_data.content)
 
 
 
@@ -8,3 +14,7 @@
 
 
 #This is where we will load
+
+
+#This is where we run the code
+get_launch_date('https://api.spacexdata.com/v3/launches')
